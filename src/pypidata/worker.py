c074b26ea0bf7c003296c20bd8b80e83560fc56b
Paul Moore <p.f.moore@gmail.com>
2021-05-12 14:48:27 +0100

    Add example of more complex workers

--- src/pypidata/worker.py
+++ src/pypidata/worker.py
@@ -1,7 +1,9 @@
 from concurrent.futures import ThreadPoolExecutor
-from typing import Iterable
-from queue import Queue
+from typing import Iterable, List, Tuple
+import queue
 from threading import Thread
+import json
+import zlib
 
 import httpx
 from .pypi import get_project
@@ -9,7 +11,7 @@ from loguru import logger
 
 class ProducerConsumer:
     def __init__(self, producer, consumer):
-        self.queue = Queue()
+        self.queue = queue.Queue()
         self.producer = producer
         self.consumer = consumer
 
@@ -18,6 +20,13 @@ class ProducerConsumer:
             data = self.queue.get()
             if data is None:
                 break
+            # Batch up all pending data
+            data = [data]
+            while True:
+                try:
+                    data.append(self.queue.get_nowait())
+                except queue.Empty:
+                    break
             self.consumer(data)
 
     def producer_task(self, input):
@@ -45,18 +54,39 @@ class ProducerConsumer:
                 raise
 
 
+def producer(name: str):
+    simple_data = get_project(name, "simple", client)
+    json_data = get_project(name, "json", client)
+    serial = json.loads(json_data).get("last_serial")
+    yield name, serial, json_data, simple_data
+
+def consumer(items: List[Tuple[str, int, bytes, bytes]]):
+    conn.execute(
+        pypi_data.insert(),
+        [
+            {
+                "name": name,
+                "serial": serial,
+                "json_data": zlib.compress(json_data),
+                "simple_data": zlib.compress(simple_data),
+            }
+            for name, serial, json_data, simple_data in items
+        ]
+
+    )
+
 def get_project_data(names: Iterable[str]):
 
     with httpx.Client() as client:
         def p(name):
             yield get_project(name, "simple", client)
             yield get_project(name, "json", client)
-        def c(data):
-            name, type, data = data
-            if data is None:
-                logger.error(f"{name}: Data is None")
-            else:
-                logger.info(f"DATA: {name}, {type}, {len(data)}")
+        def c(items):
+            for name, type, data in items:
+                if data is None:
+                    logger.error(f"{name}: Data is None")
+                else:
+                    logger.info(f"DATA: {name}, {type}, {len(data)}")
         pc = ProducerConsumer(p, c)
         pc.start(names)
 
