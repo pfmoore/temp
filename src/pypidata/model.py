b58b4b2d17a928ef57008009cb3ebf491bc09d28
Paul Moore <p.f.moore@gmail.com>
2021-05-12 10:42:58 +0100

    Use sqlalchemy core, not ORM

--- src/pypidata/model.py
+++ src/pypidata/model.py
@@ -1,47 +1,44 @@
 from sqlalchemy.orm import declarative_base, Session
-from sqlalchemy import Column, String, Integer, LargeBinary, DateTime
+from sqlalchemy import MetaData, Table, Column, String, Integer, LargeBinary, DateTime
 import zlib
 
-Base = declarative_base()
+metadata = MetaData()
 
-class JsonData(Base):
-    __tablename__ = "json_data"
-    name = Column(String, primary_key=True)
-    serial = Column(Integer, nullable=False)
-    data = Column(LargeBinary)        
+pypi_data = Table("pypi_data", metadata,
+    Column("name", String, primary_key=True),
+    Column("serial", Integer, nullable=False),
+    Column("json_data", LargeBinary),
+    Column("simple_data", LargeBinary),
+)
 
-class SimpleData(Base):
-    __tablename__ = "simple_data"
-    name = Column(String, primary_key=True)
-    serial = Column(Integer, nullable=False)
-    data = Column(LargeBinary)
+packages = Table("packages", metadata,
+    Column("name", String, primary_key=True),
+    Column("display_name", String),
+    Column("last_serial", Integer, nullable=False),
+)
 
-class Package(Base):
-    __tablename__ = "packages"
-    name = Column(String, primary_key=True)
-    display_name = Column(String)
-    last_serial = Column(Integer, nullable=False)
-
-class Changelog(Base):
-    __tablename__ = "changelog"
-    name = Column(String)
-    display_name = Column(String)
-    serial = Column(Integer, primary_key=True)
-    version = Column(String)
-    timestamp = Column(DateTime)
-    action = Column(String)
+changelog = Table("changelog", metadata,
+    Column("name", String),
+    Column("display_name", String),
+    Column("serial", Integer, primary_key=True),
+    Column("version", String),
+    Column("timestamp", DateTime),
+    Column("action", String),
+)
 
 if __name__ == "__main__":
     from sqlalchemy import create_engine
     engine = create_engine('sqlite:///foo.db', echo=True)
-    Base.metadata.create_all(engine)
+    metadata.create_all(engine)
     examples = [
         ("test", 1, b"Hello, world"),
         ("example", 2, b"Another set of data")
     ]
-    with Session(engine) as session:
-        with session.begin():
-            for name, serial, data in examples:
-                data = zlib.compress(data)
-                obj = JsonData(name=name, serial=serial, data=data)
-                session.add(obj)
+    with engine.begin() as conn:
+        conn.execute(
+            pypi_data.insert(),
+            [
+                dict(name=name, serial=serial, json_data=zlib.compress(data))
+                for name, serial, data in examples
+            ]
+        )
